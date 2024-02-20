b1c5d4775c13b04997d6d65e1fa2bd2b0b515167
Eamon Heffernan <eamon.heffernan@finplex.com.au>
2024-02-20 14:22:27 +1100

    Tabs in state

--- src/components/tabs.tsx
+++ src/components/tabs.tsx
@@ -1,9 +1,15 @@
-import { component$, useComputed$ } from "@builder.io/qwik";
-import { Link, useLocation } from "@builder.io/qwik-city";
+import {
+  component$,
+  useComputed$,
+  useSignal,
+  useTask$,
+} from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
+import { isServer } from "@builder.io/qwik/build";
 
 type TabConfig = {
   label: string;
-  href: string;
+  filter: string;
   metricTons: number;
   cost: number;
 };
@@ -13,6 +19,22 @@ interface TabsProps {
 
 export const DieselTabs = component$((props: TabsProps) => {
   const loc = useLocation();
+  const initialTab = loc.url.searchParams.get("filter");
+
+  const selectedTab = useSignal(initialTab);
+
+  useTask$(({ track }) => {
+    const tab = track(selectedTab);
+
+    if (isServer) return;
+
+    const url = new URL(window.location.href);
+
+    if (tab) url.searchParams.set("filter", tab);
+    else url.searchParams.delete("filter");
+
+    window.history.replaceState({ path: url.toString() }, "", url.toString());
+  });
 
   const maxTonnage = useComputed$(() => {
     return props.tabs.reduce((acc, tab) => {
@@ -24,11 +46,14 @@ export const DieselTabs = component$((props: TabsProps) => {
     <div class="w-full border-b border-t border-gray-200 py-2 text-center text-sm font-medium ">
       <ul class="-mb-px flex w-full flex-wrap justify-between divide-x">
         {props.tabs.map((tab) => (
-          <Link
+          <button
             class={["relative flex-1 px-2"]}
-            key={tab.href}
-            href={tab.href}
-            aria-current={loc.url.search === tab.href ? "page" : undefined}
+            key={tab.filter}
+            aria-current={selectedTab.value === tab.filter ? "page" : undefined}
+            onClick$={() => {
+              if (selectedTab.value === tab.filter) selectedTab.value = null;
+              else selectedTab.value = tab.filter;
+            }}
             style={{
               "--oil-height": `${Math.max(
                 0,
@@ -45,7 +70,7 @@ export const DieselTabs = component$((props: TabsProps) => {
                 "relative inline-block w-full flex-1 p-4 text-black before:absolute  before:bottom-0 before:left-0 before:-z-10 before:h-[var(--oil-height)] before:w-full before:bg-stone-200 before:content-['']",
                 {
                   "active inline-block bg-stone-700 p-4 !text-white before:bg-stone-600 ":
-                    loc.url.search === tab.href,
+                    selectedTab.value === tab.filter,
                 },
               ]}
               key={tab.label}
@@ -60,7 +85,7 @@ export const DieselTabs = component$((props: TabsProps) => {
                 })}
               </p>
             </li>
-          </Link>
+          </button>
         ))}
       </ul>
     </div>
