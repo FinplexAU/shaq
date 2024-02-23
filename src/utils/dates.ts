4d206bfd0ec4a8c590d62023d447e827193dcfb6
Eamon Heffernan <eamon.heffernan@finplex.com.au>
2024-02-23 19:35:01 +1100

    Fix date strings

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,6 +1,7 @@
-export const dateString = (date: Date) => {
+export const dateString = (date?: Date) => {
+  if (!date) return "";
   const year = date.getUTCFullYear();
-  const month = date.getUTCMonth().toString().padStart(2, "0");
+  const month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
   const day = date.getUTCDate().toString().padStart(2, "0");
   return `${year}-${month}-${day}`;
 };
