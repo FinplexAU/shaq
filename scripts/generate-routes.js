28ac691b72f9e5b5d015e5a7557120c9e17c5bb4
Eamon Heffernan <eamon.heffernan@finplex.com.au>
2024-02-23 16:42:02 +1100

    Set headers file in script

--- scripts/generate-routes.js
+++ scripts/generate-routes.js
@@ -1,10 +1,11 @@
-import { readdirSync, statSync, writeFileSync } from "fs";
+import { readFileSync, readdirSync, statSync, writeFileSync } from "fs";
 
 const generateExclude = (path) => {
   const folder = readdirSync("./dist" + path);
   const exclude = [];
   for (const item of folder) {
     if (item === "build") continue;
+    if (item === "assets") continue;
     if (item.startsWith("_")) continue;
 
     const itemPath = path + item;
@@ -19,7 +20,9 @@ const generateExclude = (path) => {
   return exclude;
 };
 
-const exclude = ["/build/*", "/assets/*", ...generateExclude("/")];
+const generatedExcludes = generateExclude("/");
+
+const exclude = ["/build/*", "/assets/*", ...generatedExcludes];
 
 const data = {
   version: 1,
@@ -28,3 +31,16 @@ const data = {
 };
 
 writeFileSync("./dist/_routes.json", JSON.stringify(data));
+const y = readFileSync("./dist/_headers");
+const x = y.toString().split("\n");
+console.log(x);
+
+for (const exclude of generatedExcludes) {
+  x.push(exclude);
+  x.push(
+    "  Cache-Control: public, max-age=3600, s-maxage=3600, stale-while-revalidate=604800",
+  );
+
+  x.push("");
+}
+writeFileSync("./dist/_headers", x.join("\n"));
