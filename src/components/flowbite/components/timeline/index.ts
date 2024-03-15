7737157eddfc020a30f506c53f31aa1214796268
Eamon Heffernan <eamon.heffernan@finplex.com.au>
2024-03-15 13:24:08 +1100

    Add re-exports for timeline

---
+++ src/components/flowbite/components/timeline/index.ts
@@ -0,0 +1,7 @@
+export { default as Timeline } from "./timeline";
+export { default as TimelineBody } from "./timeline-body";
+export { default as TimelineContent } from "./timeline-content";
+export { default as TimelineItem } from "./timeline-item";
+export { default as TimelinePoint } from "./timeline-point";
+export { default as TimelineTime } from "./timeline-time";
+export { default as TimelineTitle } from "./timeline-title";
