9868e171dc7a66117f350938637388dac834683b
Eamon Heffernan <eamon.heffernan@finplex.com.au>
2024-03-20 18:23:51 +1100

    Ship Stuff

--- src/routes/v2/map/index.tsx
+++ src/routes/v2/map/index.tsx
@@ -3,95 +3,72 @@ import * as am5 from "@amcharts/amcharts5";
 import am5geodata_worldLow from "@amcharts/amcharts5-geodata/worldLow";
 import * as am5map from "@amcharts/amcharts5/map";
 import colors from "tailwindcss/colors";
+import rawShipData from "@/public/ship-data.json";
+import { routeLoader$ } from "@builder.io/qwik-city";
+
+export const useShipData = routeLoader$(async () => {
+	return rawShipData;
+});
 
 export default component$(() => {
-  return (
-    <div class="grid flex-1 grid-cols-3 place-items-center  gap-4">
-      <div class="col-span-2 h-full w-full ">
-        <GlobeVis></GlobeVis>
-      </div>
-      <h1 class="mr-auto pb-24 text-center text-4xl font-semibold">
-        Shipment Tracking <br /> Coming Soon
-      </h1>
-    </div>
-  );
+	return <GlobeVis></GlobeVis>;
 });
 
 const GlobeVis = component$(() => {
-  // eslint-disable-next-line qwik/no-use-visible-task
-  useVisibleTask$(({ cleanup }) => {
-    const root = am5.Root.new("chart-div");
-    const chart = root.container.children.push(
-      am5map.MapChart.new(root, {
-        projection: am5map.geoOrthographic(),
-        panX: "rotateX",
-        panY: "rotateY",
-        wheelX: "none",
-        wheelY: "none",
-        pinchZoom: true,
-        paddingBottom: 50,
-        paddingTop: 50,
-      }),
-    );
+	const data = useShipData();
+	// eslint-disable-next-line qwik/no-use-visible-task
+	useVisibleTask$(({ cleanup }) => {
+		console.log(data.value);
+		const root = am5.Root.new("chart-div");
+		const chart = root.container.children.push(
+			am5map.MapChart.new(root, {
+				panX: "rotateX",
+			})
+		);
 
-    // Create series for background fill
-    // https://www.amcharts.com/docs/v5/charts/map-chart/map-polygon-series/#Background_polygon
-    const backgroundSeries = chart.series.push(
-      am5map.MapPolygonSeries.new(root, {}),
-    );
-    backgroundSeries.mapPolygons.template.setAll({
-      fill: am5.color(colors.sky[700]),
-      fillOpacity: 1,
-      strokeOpacity: 0,
-    });
+		root.container.children.push(chart);
 
-    // Add background polygon
-    // https://www.amcharts.com/docs/v5/charts/map-chart/map-polygon-series/#Background_polygon
-    backgroundSeries.data.push({
-      geometry: am5map.getGeoRectangle(90, 180, -90, -180),
-    });
+		chart.series.push(
+			am5map.MapPolygonSeries.new(root, {
+				geoJSON: am5geodata_worldLow,
+				exclude: ["AQ"],
+				fill: am5.color(colors.slate[200]),
+				stroke: am5.color(colors.slate[400]),
+			})
+		);
 
-    root.container.children.push(chart);
+		const pointSeries = chart.series.push(am5map.MapPointSeries.new(root, {}));
 
-    chart.series.push(
-      am5map.MapPolygonSeries.new(root, {
-        geoJSON: am5geodata_worldLow,
-        fill: am5.color(colors.stone[200]),
-        stroke: am5.color(colors.stone[400]),
-      }),
-    );
+		pointSeries.bullets.push(function () {
+			const sprite = am5.Circle.new(root, {
+				radius: 7,
+				tooltipY: 0,
+				fill: am5.color(0xffba00),
+				tooltipText: "{title}",
+			});
 
-    // Create point series for markers
-    // https://www.amcharts.com/docs/v5/charts/map-chart/map-point-series/
-    const pointSeries = chart.series.push(am5map.MapPointSeries.new(root, {}));
+			return am5.Bullet.new(root, {
+				sprite,
+			});
+		});
 
-    pointSeries.bullets.push(function () {
-      const circle = am5.Triangle.new(root, {
-        height: 15,
-        width: 15,
-        tooltipY: 0,
-        fill: am5.color(0xffba00),
-        tooltipText: "{title}",
-      });
+		const coords = data.value.features[0]?.geometry.coordinates[0];
 
-      return am5.Bullet.new(root, {
-        sprite: circle,
-      });
-    });
+		if (coords) {
+			pointSeries.data.push({
+				geometry: {
+					type: "Point",
+					coordinates: coords[coords.length - 1],
+				},
+				title: data.value.properties.vessel.name,
+			});
+		}
 
-    pointSeries.data.push({
-      geometry: {
-        type: "Point",
-        coordinates: [-37.813629, 144.963058].reverse(),
-      },
-      title: "Melbourne",
-    });
+		chart.appear(1000, 100);
 
-    chart.appear(1000, 100);
-
-    cleanup(() => {
-      root.dispose();
-    });
-  });
-  return <div class="h-full w-full flex-1" id="chart-div"></div>;
+		cleanup(() => {
+			root.dispose();
+		});
+	});
+	return <div class="h-full w-full flex-1" id="chart-div"></div>;
 });
