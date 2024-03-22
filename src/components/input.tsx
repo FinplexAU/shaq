e45d2cc503e47a03fc145e67ee420d2f3db28a58
Eamon Heffernan <eamon.heffernan@finplex.com.au>
2024-03-22 19:22:23 +1100

    New inputs

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,12 +1,17 @@
-import { type PropsOf, component$ } from "@builder.io/qwik";
+import { type PropsOf, component$, useComputed$ } from "@builder.io/qwik";
 import { cn } from "./flowbite/helpers";
+import { omitProps } from "@builder.io/qwik-labs";
 
 export const Input = component$(
-	({
-		error,
-		class: propClass,
-		...props
-	}: PropsOf<"input"> & { error?: string }) => {
+	(props: PropsOf<"input"> & { error?: string | string[] }) => {
+		const error = useComputed$(
+			() =>
+				props.error &&
+				(Array.isArray(props.error) ? props.error.at(0) : props.error)
+		);
+
+		const { class: className, ...rest } = props;
+
 		return (
 			<div
 				class={[
@@ -21,16 +26,18 @@ export const Input = component$(
 					{props.title}
 				</label>
 				<input
-					{...props}
+					{...rest}
 					class={cn([
 						"block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500",
-						propClass,
+						className,
 						{
-							"outline outline-red-700": error,
+							"outline outline-red-700": !!error.value,
 						},
 					])}
 				/>
-				{error && <span class="absolute text-sm text-red-500">{error}</span>}
+				{error.value && (
+					<span class="absolute text-sm text-red-500">{error.value}</span>
+				)}
 			</div>
 		);
 	}
