2b99f12eb4ba5382f76fc757eaf3c845bf4f151c
Nexon <nick.j.exon@gmail.com>
2024-03-28 23:25:29 +1100

    fix of tabs

--- src/routes/v2/contract/[id]/cycles/index.tsx
+++ src/routes/v2/contract/[id]/cycles/index.tsx
@@ -23,7 +23,6 @@ import { HiMinusSolid, HiPlusSolid } from "@qwikest/icons/heroicons";
 import { eq } from "drizzle-orm";
 import moment from "moment-timezone";
 import { Button } from "~/components/button";
-import Debugger from "~/components/debugger";
 import { cn } from "~/components/flowbite/helpers";
 import { Input } from "~/components/input";
 import { drizzleDb } from "~/db/db";
@@ -57,7 +56,91 @@ export const useCycles = routeLoader$(async (ev) => {
 		.map((workflow) => workflow.cycle)
 		.filter((cycle): cycle is Exclude<typeof cycle, null> => Boolean(cycle));
 
-	return cycles;
+	const totals = {
+		"3-months": {
+			volume: 0,
+			totalCost: 0,
+		},
+		"next-month": {
+			volume: 0,
+			totalCost: 0,
+		},
+		"this-month": {
+			volume: 0,
+			totalCost: 0,
+		},
+		"in-transit": {
+			volume: 0,
+			totalCost: 0,
+		},
+		"settled-this-month": {
+			volume: 0,
+			totalCost: 0,
+		},
+		"settled-this-year": {
+			volume: 0,
+			totalCost: 0,
+		},
+		landed: {
+			volume: 0,
+			totalCost: 0,
+		},
+	};
+	const sortedCycles: Record<CycleDateGroup, Array<(typeof cycles)[number]>> = {
+		"3-months": [],
+		"next-month": [],
+		"this-month": [],
+		"in-transit": [],
+		"settled-this-month": [],
+		"settled-this-year": [],
+		landed: [],
+	};
+
+	const add = (key: CycleDateGroup, cycle: (typeof cycles)[number]) => {
+		sortedCycles[key].push(cycle);
+		totals[key].volume += Number(cycle.volume);
+		totals[key].totalCost += Number(cycle.cost);
+	};
+
+	for (const cycle of cycles) {
+		const m = moment(
+			cycle.settlementDate ? cycle.settlementDate : cycle.expectedDate
+		);
+
+		if (m.isAfter(moment().add(3, "month"))) {
+			// 3-months
+			add("3-months", cycle);
+			continue;
+		}
+		const nextMonth = moment().add(1, "month");
+
+		if (
+			m.isBetween(nextMonth.clone().startOf("month"), nextMonth.endOf("month"))
+		) {
+			// next month
+			add("next-month", cycle);
+			continue;
+		}
+		if (
+			m.isBetween(moment().startOf("month"), moment().endOf("month").valueOf())
+		) {
+			// this month
+			add("this-month", cycle);
+			continue;
+		}
+		if (m.isBetween(moment().startOf("month"), moment())) {
+			// settled this month
+			add("settled-this-month", cycle);
+			continue;
+		}
+		if (m.isBetween(moment().startOf("year"), moment())) {
+			// settled this year
+			add("settled-this-year", cycle);
+			continue;
+		}
+	}
+
+	return { all: cycles, sorted: sortedCycles, totals };
 });
 
 export const useCreateCycle = routeAction$(
@@ -95,68 +178,30 @@ export const useCreateCycle = routeAction$(
 	},
 	zod$({
 		expectedDate: z.coerce.date(),
-		cost: z.coerce.number(),
-		volume: z.coerce.number(),
+		cost: z.coerce.number().optional(),
+		volume: z.coerce.number().optional(),
 	})
 );
 
 export const CyclesContext = createContextId<{
 	filter: Signal<string>;
-	// totals: { volume: number; avgPrice: number };
 }>("cycles-index");
+
+type CycleDateGroup =
+	| "3-months"
+	| "next-month"
+	| "this-month"
+	| "settled-this-month"
+	| "in-transit"
+	| "settled-this-year"
+	| "landed";
 
 export default component$(() => {
 	const loc = useLocation();
 	const cycles = useCycles();
 	const createCycle = useCreateCycle();
 	const showCycleCreate = useSignal(false);
-	const filter = useSignal("in-transit");
-
-	const filteredCycles = useComputed$(() => {
-		const dateFilter = (date: Date, dateType: "settled" | "expected") => {
-			const m = moment(date);
-			switch (filter.value) {
-				case "3-months":
-					return m.isAfter(moment().add(3, "month"));
-				case "next-month":
-					// expected between the first and last of next month
-					const nextMonth = moment().add(1, "month");
-					return m.isBetween(
-						nextMonth.startOf("month"),
-						nextMonth.endOf("month")
-					);
-				case "this-month":
-					// settled this month
-					return m.isSameOrBefore(moment().endOf("month").valueOf());
-				case "settled-this-month":
-					if (dateType === "settled")
-						return m.isBetween(moment().startOf("month"), moment());
-					break;
-				case "settled-this-year":
-					if (dateType === "settled")
-						return m.isBetween(moment().startOf("year"), moment());
-					break;
-				default:
-					return false;
-			}
-		};
-
-		return cycles.value.filter((cycle) => {
-			return cycle.settlementDate
-				? dateFilter(cycle.settlementDate, "settled")
-				: dateFilter(cycle.expectedDate, "expected");
-		});
-	});
-
-	// const totals = useComputed$(() => {
-	// 	return filteredCycles.value.reduce<{ volume: number; avgPrice: number }>(
-	// 		(a, b, i) => ({
-	// 			volume: a.volume + Number(b.volume),
-	// 			avgPrice: (a.volume + Number(b.cost)) / (i + 1),
-	// 		}),
-	// 		{ volume: 0, avgPrice: 0 }
-	// 	);
-	// });
+	const filter = useSignal<CycleDateGroup>("in-transit");
 
 	useContextProvider(CyclesContext, { filter });
 
@@ -182,7 +227,12 @@ export default component$(() => {
 				<Button type="submit">Create a new trade cycle</Button>
 			</Form> */}
 			<div class="flex flex-col gap-2 p-4">
-				{filteredCycles.value.map((cycle) => (
+				<div class="grid grid-cols-4 gap-2 text-xs">
+					<p>Date</p>
+					<p>Cost</p>
+					<p>Volume (MT)</p>
+				</div>
+				{cycles.value.sorted[filter.value].map((cycle) => (
 					<AppLink
 						route="/v2/contract/[id]/cycles/[cycleId]/"
 						param:id={loc.params.id!}
@@ -190,19 +240,32 @@ export default component$(() => {
 						key={cycle.id}
 					>
 						{/* {cycle.settlementDate} */}
-						<div class="rounded border bg-neutral-50 p-2 shadow hover:bg-neutral-200">
-							<p>Expected: {cycle.expectedDate.toLocaleDateString([])}</p>
-							<p>Settles: {cycle.settlementDate?.toLocaleDateString([])}</p>
-							{/* <p>{props.contract.}</p> */}
+						<div class="grid grid-cols-4 gap-2 rounded border bg-neutral-50 p-2 shadow hover:bg-neutral-200">
+							{cycle.settlementDate ? (
+								<p>{cycle.settlementDate.toLocaleDateString("en-AU")}</p>
+							) : (
+								<p>{cycle.expectedDate.toLocaleDateString("en-AU")}</p>
+							)}
+							<p>
+								{cycle.cost
+									? Number(cycle.cost).toLocaleString("en-AU", {
+											currency: "AUD",
+											style: "currency",
+										})
+									: "-"}
+							</p>
+							<p>
+								{cycle.volume ? Number(cycle.volume).toLocaleString() : "-"}
+							</p>
 						</div>
 					</AppLink>
 				))}
 			</div>
 			{showCycleCreate.value && (
-				<div class="absolute bottom-12 right-12 z-10 rounded-lg border bg-white p-2 shadow">
+				<div class="absolute bottom-14 right-14 z-10 w-96 rounded-lg border bg-white p-4 shadow">
 					<Form
 						action={createCycle}
-						class="flex flex-col gap-2"
+						class="flex flex-col gap-4"
 						onSubmit$={() => {
 							showCycleCreate.value = false;
 						}}
@@ -224,7 +287,7 @@ export default component$(() => {
 				onClick$={() => {
 					showCycleCreate.value = !showCycleCreate.value;
 				}}
-				class="absolute bottom-6 right-6 z-20 grid h-12 w-12 cursor-pointer place-items-center rounded-full bg-gradient-to-tr from-green-50 via-green-100 to-blue-200 shadow-lg outline-blue-400 hover:brightness-95 active:outline active:[&>*]:text-blue-700"
+				class="absolute bottom-6 right-6 z-20 grid h-12 w-12 cursor-pointer place-items-center rounded-full bg-gradient-to-tr from-green-50 via-green-100 to-blue-200 shadow-xl outline-blue-400 hover:brightness-95 active:outline active:[&>*]:text-blue-700"
 			>
 				{showCycleCreate.value ? (
 					<HiMinusSolid class="text-xl"></HiMinusSolid>
@@ -245,29 +308,63 @@ export const CycleTab = component$(
 		onClick$: propClick,
 		filter,
 		...props
-	}: PropsOf<"div"> & { filter: string }) => {
-		const cycles = useContext(CyclesContext);
+	}: PropsOf<"div"> & { filter: CycleDateGroup }) => {
+		const displayContext = useContext(CyclesContext);
+		const cycles = useCycles();
+		const total = useComputed$(() => cycles.value.totals[filter]);
+		const oilHeight = useComputed$(() => {
+			const max = Math.max(
+				...Object.values(cycles.value.totals).map((total) => total.volume)
+			);
+
+			return (total.value.volume / (max * 1.3)) * 100;
+		});
+
 		return (
 			<div
 				onClick$={[
 					propClick,
 					$(() => {
-						cycles.filter.value = filter;
+						displayContext.filter.value = filter;
 					}),
 				]}
 				class={cn(
 					[
-						"flex-1 cursor-pointer rounded border py-2 text-center text-sm",
+						"relative flex-1 cursor-pointer overflow-hidden rounded border bg-neutral-500 py-2 text-center text-sm font-medium text-white shadow",
 						{
 							" border-blue-300  bg-gradient-to-tr from-green-50 via-green-100 to-blue-200 text-blue-700":
-								cycles.filter.value === filter,
+								displayContext.filter.value === filter,
 						},
 					],
 					propClass
 				)}
 				{...props}
 			>
-				<Slot></Slot>
+				<div class="relative z-10 ">
+					<Slot></Slot>
+					<p class="text-xs">
+						{total.value.totalCost.toLocaleString("en-AU", {
+							style: "currency",
+							currency: "AUD",
+							notation: "compact",
+						})}
+					</p>
+					<p class="text-xs">
+						{total.value.volume.toLocaleString("en-AU", {
+							notation: "compact",
+						})}{" "}
+						mt
+					</p>
+				</div>
+				<div
+					style={{ "--oil-height": `${oilHeight.value}%` }}
+					class={[
+						"absolute bottom-0 left-0 h-[var(--oil-height)] w-full bg-gradient-to-t from-neutral-900 to-neutral-700",
+						{
+							"opacity-20": displayContext.filter.value === filter,
+						},
+					]}
+				></div>
 			</div>
 		);
 	}
